46f12573c6443e91a774dcc12c7ce999000dddb1
luisgomez7t <162279891+7tCam@users.noreply.github.com>
2024-08-03 01:31:05 +0000

    GeneradorDePerfil

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
+import { Collapse } from "bootstrap";
 import "../style/index.css";
+import { right } from "@popperjs/core";
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
@@ -26,21 +28,30 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  let cover = `<div class="cover"><img src="${variables.background}"/></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  // let
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <img src="${variables.avatarURL}" class="photo"/>
+          <h1>${variables.name || "Nombre"} ${variables.lastName ||
+    "Apellido"}</h1>
+          <h2>${variables.role || "Especialidad"}</h2>
+          <h3>${variables.city || "Ciudad"} ,${variables.country || "País"}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +65,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://www.starkcloud.com/hubfs/Imported_Blog_Media/Que-es-Ciberseguridad.webp",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhmFaFXdrPZEeqXNxGTP9PgPJ_VYWC_RD30A&s",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
